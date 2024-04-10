02c1a0c01c0bc76f12d4b4667d36b5b093eaf281
adollking <gamershohe@gmail.com>
2024-04-11 02:32:23 +0700

    feat: add fontawsome

--- src/views/Dasboard.vue
+++ src/views/Dasboard.vue
@@ -16,25 +16,30 @@ export default {
                 { id: 3, name: "Buy Cheese", color: "blue" },
                 { id: 4, name: "Buy Butter", color: "yellow" },
             ],
+            ongoing: [],
+            done: [],
             list2: [
-                { id: 5, name: "Buy Milk", color: "red" },
-                { id: 6, name: "Buy Bread", color: "green" },
-                { id: 7, name: "Buy Cheese", color: "blue" },
-                { id: 8, name: "Buy Butter", color: "yellow" },
+                { id: 5, name: "Buy sarwama", color: "red" },
+                { id: 6, name: "Buy burger", color: "green" },
+                { id: 7, name: "Buy Cheese a", color: "blue" },
+                { id: 8, name: "Buy Butterfly", color: "yellow" },
             ],
         };
     },
 
     methods: {
         clone({ name }) {
-            return { name, id: idGlobal++ };
+            return { name, id: Date.now() };
         },
         pullFunction() {
             return this.controlOnStart ? "clone" : true;
         },
         start({ originalEvent }) {
             this.controlOnStart = originalEvent.ctrlKey;
-        }
+        },
+        addNew() {
+            this.todo.push({ id: 9, name: "Buy Butter", color: "yellow" });
+        },
     }
 };
 
@@ -43,29 +48,40 @@ export default {
 <template>
     <Layout>
     </Layout>
-    <div class="mb-4">
-        <b-avatar></b-avatar>
-        <b-avatar variant="primary" text="BV"></b-avatar>
-        <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
-        <b-avatar variant="success" icon="people-fill"></b-avatar>
-    </div>
-
     <div class="container">
         <div class="row mt-5">
+            <div class="col-1 text-end">
+                <button class="btn btn-primary btn-sm mt-4" @click="addNew()">
+                    <i class="fa fa-plus"></i>
+                </button>
+            </div>
             <div class="col-3">
                 <h3>Todo</h3>
-                <draggable class="list-group" :list="todo" group="people" itemKey="name">
-                    <template #item="{ element, index }">
-                        <div class="list-group-item">{{ element.name }} {{ index }}</div>
+                <draggable class="dragArea list-group" :list="todo" :group="{ name: 'people', pull: pullFunction }"
+                    itemKey="name" @start="start" :clone="clone" :pull="pullFunction">
+                    <template #item="{ element }">
+                        <div class="list-group-item">{{ element.name }}</div>
                     </template>
                 </draggable>
             </div>
 
             <div class="col-3">
                 <h3>On going</h3>
-                <draggable class="list-group" :list="list2" group="people" @change="log" itemKey="name">
-                    <template #item="{ element, index }">
-                        <div class="list-group-item">{{ element.name }} {{ index }}</div>
+                <draggable class="dragArea list-group" :list="ongoing" :group="{ name: 'people', pull: pullFunction }"
+                    itemKey="name" @start="start" :clone="clone" :pull="pullFunction">
+                    <template #item="{ element }">
+                        <div class="list-group-item">{{ element.name }}</div>
+
+                    </template>
+                </draggable>
+            </div>
+
+            <div class="col-3">
+                <h3>Done</h3>
+                <draggable class="dragArea list-group" :list="list2" :group="{ name: 'people', pull: pullFunction }"
+                    itemKey="name" @start="start" :clone="clone" :pull="pullFunction">
+                    <template #item="{ element }">
+                        <div class="list-group-item">{{ element.name }}</div>
                     </template>
                 </draggable>
             </div>
